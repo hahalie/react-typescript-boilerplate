5598aaf2ae4e1d744076d1b7355a0bd8c644532f
hahalie <tereuspark@yahoo.com>
2019-12-10 15:57:26 -0600

    Initial Commit
    React boilerplate application integrated with Typescript and basic webpack & babel configuration.

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export default function App(): JSX.Element {
+  return <div>Hello World!</div>;
+}
+
+const root = document.getElementById("root");
+
+ReactDOM.render(<App />, root);
